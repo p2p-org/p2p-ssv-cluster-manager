8483b97b26afba40db1b290426567d30ee7b5196
Mikhail Nacharov <mikhail.nacharov@p2p.org>
2024-08-19 11:13:20 +0200

    fix(EI-540): disable logging to file

--- scripts/common/helpers/logger.ts
+++ scripts/common/helpers/logger.ts
@@ -2,12 +2,11 @@ import log4js from 'log4js'
 
 log4js.configure({
   appenders: {
-    file: { type: 'file', filename: 'logs.txt' },
     console: {
       type: 'console',
     },
   },
-  categories: { default: { appenders: ['console', 'file'], level: 'info' } },
+  categories: { default: { appenders: ['console'], level: 'info' } },
 })
 
 export const logger = log4js.getLogger()
